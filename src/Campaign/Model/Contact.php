d3617f9814310159e5f75c438a577179f4306393
marcaube <marc@tourismecie.com>
2013-12-18 10:26:50 -0500

    Add extra data in contacts

--- src/Campaign/Model/Contact.php
+++ src/Campaign/Model/Contact.php
@@ -10,10 +10,50 @@ class Contact
     protected $email;
 
     /**
+     * @var array
+     */
+    protected $columns = array();
+
+    /**
      * @param Email $email
      */
     public function __construct(Email $email)
     {
         $this->email = $email;
+    }
+
+    /**
+     * @param string $name
+     * @param string $value
+     *
+     * @return Contact
+     */
+    public function setColumn($name, $value)
+    {
+        $this->columns[$name] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getColumns()
+    {
+        return $this->columns;
+    }
+
+    /**
+     * @param $name
+     *
+     * @return string|null
+     */
+    public function getColumn($name)
+    {
+        if (!array_key_exists($name, $this->columns)) {
+            return null;
+        }
+
+        return $this->columns[$name];
     }
 }
---
+++ tests/Campaign/Model/ContactTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use Campaign\Model\Contact;
+
+class ContactTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_you_can_set_columns()
+    {
+        $email = $this->getMockBuilder('Campaign\Model\Email')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $contact = new Contact($email);
+        $contact->setColumn('name', 'value');
+
+        $this->assertEquals(
+            array('name' => 'value'),
+            $contact->getColumns()
+        );
+
+        $this->assertEquals(
+            'value',
+            $contact->getColumn('name')
+        );
+
+        $this->assertNull($contact->getColumn('foo'));
+    }
+}
